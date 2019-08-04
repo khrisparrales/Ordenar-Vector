8497c9175e13d8bd8a448e25373878666e98060a
lecristianot <lecristianot@gmail.com>
2019-08-04 11:11:12 -0500

    Creacion del Proyecto

---
+++ Ordenar-Vector/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ordenar_Vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] vector = { 4, 5, 6, 1, 2, 3, 4, 12, 6, 4, 2, 1, 5, 8, 9 };
+            OrdenarDesc(vector);
+            foreach (int valor in vector)
+            {
+                Console.Write(valor+" ");
+            }
+            Console.ReadKey();
+        }
+        public static int[] OrdenarDesc(int[] Ma, int n=0)
+        {
+            if (Ma.Length == n) { return Ma; }
+            int aux = Ma[n];
+            for (int i = n; i < Ma.Length; i++)
+            {
+                if (aux < Ma[i]) { aux = Ma[i]; Ma[i] = Ma[n]; Ma[n] = aux; }
+            }
+            return OrdenarDesc(Ma, n + 1);
+        }
+        public static int[] OrdenarAsc(int[] Ma, int n = 0)
+        {
+            if (Ma.Length == n) { return Ma; }
+            int aux = Ma[n];
+            for (int i = n; i < Ma.Length; i++)
+            {
+                if (aux > Ma[i]) { aux = Ma[i]; Ma[i] = Ma[n]; Ma[n] = aux; }
+            }
+            return OrdenarDesc(Ma, n + 1);
+        }
+
+    }
+}
